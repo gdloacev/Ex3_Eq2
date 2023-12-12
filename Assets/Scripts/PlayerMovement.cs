75d9695f9aa59ee3f73703a342728ed870887209
alanimax25 <alan_radio@hotmail.com>
2023-12-12 17:31:31 -0600

    Corrections/Idle Animation/Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float horizontal;
+    private float vertical;
+    private Rigidbody2D rb;
+    [SerializeField] private Vector2 boxSize;
+    [SerializeField] private float castDistance;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private bool readyToJump;
+    private bool dead;
+    private int lives;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        readyToJump = true;
+        dead = false;
+        lives = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead)
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+        }
+        else if (dead)
+        {
+            horizontal = 0;
+
+        }
+
+        transform.Translate(horizontal * speed * Time.deltaTime, 0, 0);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && readyToJump)
+        {
+            if (isGrounded())
+            {
+                rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+                readyToJump = false;
+
+            }
+        }
+    }
+
+    public bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            readyToJump = true;
+        }
+    }
+}
